a54ce60c6cd17a58f3f5d89c441a90d7d8732a8b
Olivia U <olivia.yu@dasheng.com.tw>
2022-06-11 00:48:43 +0800

    修正最新消息清單

--- src/app/pages/news/news-coontent/news-coontent.component.ts
+++ src/app/pages/news/news-coontent/news-coontent.component.ts
@@ -1,4 +1,5 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, Injector, Input, OnInit } from '@angular/core';
+import { NavParams } from '@ionic/angular';
 import { BaseComponent } from 'src/app/_shared/component/base/base.component';
 
 @Component({
@@ -7,49 +8,17 @@ import { BaseComponent } from 'src/app/_shared/component/base/base.component';
   styleUrls: ['./news-coontent.component.scss'],
 })
 export class NewsContentComponent extends BaseComponent implements OnInit {
-  public newsList = [
-    {
-      "Date": "2022-04-01",
-      "PicLink": "https://www.tymetro.com.tw/tymetro-new/upload/banner/20220331112006_0.jpg",
-      "Sort": 335,
-      "Title": "【桃捷公告】4月7日起，本公司配合鐵道局工程測試，微幅調整全線時刻表說明",
-      "Top": 0
-    },
-    {
-      "Date": "2022-04-01",
-      "PicLink": "https://www.tymetro.com.tw/tymetro-new/upload/banner/20220329055848_0.jpg",
-      "Sort": 337,
-      "Title": "2022捷乘應猿─搭桃捷看Rakuten主場球賽享回程免費",
-      "Top": 1
-    },
-    {
-      "Date": "2022-04-01",
-      "PicLink": "https://www.tymetro.com.tw/tymetro-new/upload/banner/20211105044512_0.jpg",
-      "Sort": 332,
-      "Title": "親子一日票",
-      "Top": 3
-    },
-    {
-      "Date": "2022-04-01",
-      "PicLink": "https://www.tymetro.com.tw/tymetro-new/upload/banner/20210914100924_0.jpg",
-      "Sort": 304,
-      "Title": "110/9/18起 班距調整公告",
-      "Top": 8
-    },
-    {
-      "Date": "2022-04-01",
-      "PicLink": "https://www.tymetro.com.tw/tymetro-new/upload/banner/20210604022611_0.jpg",
-      "Sort": 325,
-      "Title": "中大型寵物開放搭乘機捷",
-      "Top": 12
-    }
-  ];
-  constructor(protected injector: Injector) {
+  @Input() title: string;
+  public news: any;
+  constructor(
+    protected injector: Injector,
+    protected navParams: NavParams,
+  ) {
     super(injector);
   }
 
   ngOnInit() {
-
+    this.news = this.navParams.data.newsContent;
   }
 
 
