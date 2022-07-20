02528162a1ae4f880d4014f6fc683ba2b01562e4
dannisword <dannis.word@gmail.com>
2022-07-20 19:28:59 +0800

    publish app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tymetro.ios',
-  appName: 'i桃捷2.0',
+  appName: 'i搭桃捷2.0',
   webDir: 'www',
   bundledWebRuntime: false,
   server: { "allowNavigation": ["*"] }
